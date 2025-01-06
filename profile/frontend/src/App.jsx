e6778ea86e7966bb964a3ad03c82025ae7bfe4cc
ponnana pavan <pavanponnana1@gmailcom>
2025-01-06 11:34:54 +0530

    lazy loading

--- profile/frontend/src/App.jsx
+++ profile/frontend/src/App.jsx
@@ -1,23 +1,29 @@
-import { useContext } from "react";
-import Home from "./components/Home";
-import Post from "./components/Post";
-import UserProfile from "./components/UserProfile";
+import { lazy, Suspense, useContext } from "react";
+
+const Home = lazy(() => import("./components/Home"));
+const Post = lazy(() => import("./components/Post"));
+
+const UserProfile = lazy(() => import("./components/UserProfile"));
+const UpdateProfile = lazy(() => import("./components/UpdateProfile"));
+
 import { Route, Routes } from "react-router-dom";
 import { Context } from "./components/context";
-import UpdateProfile from "./components/UpdateProfile";
+
 function App() {
   const { user } = useContext(Context);
   console.log(user);
 
   return (
-    <Routes>
-      <Route path="/" element={user ? <Home /> : <UserProfile />} />
-      <Route path="/userprofile" element={<UserProfile />} />
+    <Suspense>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <UserProfile />} />
+        <Route path="/userprofile" element={<UserProfile />} />
 
-      <Route path="/post" element={<Post />} />
+        <Route path="/post" element={<Post />} />
 
-      <Route path="/updateProfile/:userId" element={<UpdateProfile />} />
-    </Routes>
+        <Route path="/updateProfile/:userId" element={<UpdateProfile />} />
+      </Routes>
+    </Suspense>
   );
 }
 
